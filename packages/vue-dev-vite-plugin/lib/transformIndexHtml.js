36d8933f540505da343251c6eb65d8967333e0af
timor-m <timor-m@foxmail.com>
2022-03-29 17:21:53 +0800

    fix: fix vite server port

--- packages/vue-dev-vite-plugin/lib/transformIndexHtml.js
+++ packages/vue-dev-vite-plugin/lib/transformIndexHtml.js
@@ -1,6 +1,7 @@
 const { TEMPLATE_DATA, injectAssets, injectContent } = require("vue-dev-shared")
 
-const transformIndexHtml = (html) => {
+const transformIndexHtml = (html, { server }) => {
+    TEMPLATE_DATA.__HTTP_PORT__ = server.config.server.port
     return {
         html: injectContent(html, injectAssets, TEMPLATE_DATA),
         tags: []
